f1a5feae9a85628b8f7bedd3ec61031bf450dedb
Caitriona71 <caitrionaboyle@ymail.com>
2022-09-04 19:02:15 +0000

    Add variables for number of correct answers and questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 function check() {
+    var c=0;
+    var q1=document.musicquiz.question1.value;
+    var q2=document.musicquiz.question2.value;
+    var q3=document.musicquiz.question3.value;
+    var q4=document.musicquiz.question4.value;
+    var q5=document.musicquiz.question5.value;
+    var q6=document.musicquiz.question6.value;
+    var q7=document.musicquiz.question7.value;
+    var q8=document.musicquiz.question8.value;
+    var q9=document.musicquiz.question9.value;
+    var q10=document.musicquiz.question10.value;
 }
