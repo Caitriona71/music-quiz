dde344f3a655cde35818f3d4960b4e997c12ae6f
Caitriona71 <caitrionaboyle@ymail.com>
2022-09-04 19:26:06 +0000

    Add if statements for correct answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,17 @@ function check() {
     var q8=document.musicquiz.question8.value;
     var q9=document.musicquiz.question9.value;
     var q10=document.musicquiz.question10.value;
+
+    if (q1=="Treble Clef") {c++}
+if (q2=="Crotchet Rest") {c++}
+if (q3=="D Major") {c++}
+if (q4=="Timpani") {c++}
+if (q5=="Quaver Rest") {c++}
+if (q6=="4 crotchet beats in a bar") {c++}
+if (q7=="G Major") {c++}
+if (q8=="Middle C") {c++}
+if (q9=="Woodwind") {c++}
+if (q10=="Demisemiquaver Rest") {c++}
+
+document.write(c);
 }
