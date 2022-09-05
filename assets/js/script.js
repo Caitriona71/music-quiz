bb2d0fc38cd8292bad7115699a46227513c0a252
Caitriona71 <caitrionaboyle@ymail.com>
2022-09-05 14:00:47 +0000

    Edit variable c, change to variable score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 function check() {
-    var c=0;
+
+    var Score=0;
+
     var q1=document.musicquiz.question1.value;
     var q2=document.musicquiz.question2.value;
     var q3=document.musicquiz.question3.value;
@@ -10,17 +12,17 @@ function check() {
     var q8=document.musicquiz.question8.value;
     var q9=document.musicquiz.question9.value;
     var q10=document.musicquiz.question10.value;
+    
+if (q1=="Treble Clef") {Score++}
+if (q2=="Crotchet Rest") {Score++}
+if (q3=="D Major") {Score++}
+if (q4=="Timpani") {Score++}
+if (q5=="Quaver Rest") {Score++}
+if (q6=="4 crotchet beats in a bar") {Score++}
+if (q7=="G Major") {Score++}
+if (q8=="Middle C") {Score++}
+if (q9=="Woodwind") {Score++}
+if (q10=="Demisemiquaver Rest") {Score++}
 
-if (q1=="Treble Clef") {c++}
-if (q2=="Crotchet Rest") {c++}
-if (q3=="D Major") {c++}
-if (q4=="Timpani") {c++}
-if (q5=="Quaver Rest") {c++}
-if (q6=="4 crotchet beats in a bar") {c++}
-if (q7=="G Major") {c++}
-if (q8=="Middle C") {c++}
-if (q9=="Woodwind") {c++}
-if (q10=="Demisemiquaver Rest") {c++}
-
-document.write(c);
+document.write(Score);
 }
